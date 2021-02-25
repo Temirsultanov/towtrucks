7e97401d8343b7e3761e8fd0a67d8aa4e18eaa86
Мухаммад <human.twm@gmail.com>
2021-02-25 14:00:36 +0300

    Change for android 10

--- swipe.js
+++ swipe.js
@@ -22,7 +22,6 @@
             translateNumber = info.getBoundingClientRect().height - 40;
             if (window.navigator.userAgent.indexOf('Android') > -1) {
                 translateNumber = 320;
-                document.write(`translateNumber - ${translateNumber}, infoheight - ${info.getBoundingClientRect().height - 40}`)
             }
         }
         else{
