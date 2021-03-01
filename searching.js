431b51b69fca5d97d7d5d22da046f49c995e46e0
Мухаммад <human.twm@gmail.com>
2021-03-02 02:52:47 +0300

    Evacuator animation

--- searching.js
+++ searching.js
@@ -2,9 +2,20 @@
     let readyButton = document.querySelector('.ready__button');
     let searching = document.querySelector('.searching');
     let phoneInput = document.querySelector('.phoneinput');
+    let phoneinputwrap = document.querySelector('.phoneinput__wrap');
+    let drive = document.querySelector('.drive__wrap');
+    let totalCost
     let onReadyButtonClick = function () {
         phoneInput.classList.add('dn');
         searching.classList.remove('dn');
+        setTimeout(() => {
+            searching.classList.add('dn');
+            phoneinputwrap.classList.add('dn');
+            drive.classList.remove('dn');
+            window.waiting();
+        }, 5000);
     }
-    readyButton.addEventListener('click', onReadyButtonClick);
+    if (readyButton) {
+        readyButton.addEventListener('click', onReadyButtonClick);
+    }
 })();
---
+++ waiting.js
@@ -0,0 +1,19 @@
+let waiting = function () {
+    let timer = document.querySelector('.timer'), minute = 30, second = 0 ;
+    let timing = setInterval(() => {
+        if (minute === 0 && second === 1) {
+            clearInterval(timing);
+            second-=1;
+            document.querySelector('.drive').querySelector('.h1').textContent = 'Эвакуатор подъезжает'; 
+        }
+        else if (second === 0){
+            second = 59;
+            minute -=1;
+        }
+        else{
+            second-=1;
+        }
+        timer.textContent = `Осталось примерно: ${minute > 9 ? minute : '0' + minute}:${second > 9 ? second : '0' + second}`
+    }, 1000);
+};
+window.waiting = waiting;
