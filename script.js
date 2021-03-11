d74fe8585e8343076fd1ddb23851f3b12a0a9528
Мухаммад <human.twm@gmail.com>
2021-03-11 17:18:22 +0300

    Call not hide, another tgbot link etc

--- script.js
+++ script.js
@@ -9,6 +9,88 @@
     window.type = '4x4';
     window.phone = '';
     document.querySelector('.totalcost').textContent = window.totalcost + '₽';
+
+
+    // Тут содержится код свайпа менюшки вниз
+    let info = document.querySelector('.order__content');
+    let translateNumber = info.getBoundingClientRect().height - 40;
+    let where = document.querySelector('#where');
+    let from = document.querySelector('#from');
+    let onInputChange = function () {
+        window.translateNumber = info.getBoundingClientRect().height - 40;
+    }
+    from.addEventListener('change', onInputChange);
+    where.addEventListener('change', onInputChange);
+    let fromInput = document.querySelector('.input-from');
+    let whereInput = document.querySelector('.input-where');
+    // let carInput = document.querySelector('.input-car');
+    let phone = document.querySelector('.phone');
+    let total = document.querySelector('.total')
+    // Если заполнены все поля, попап с телефоном убирается и блок отодвигается вниз на другое кол-во пикселей
+    let onInput = function () {
+        
+    }
+
+    // Свайп
+    let translate = 0;
+    let startCoord = 0;
+    let curCoord = 0;
+    let closed = false;
+    let isMoving = false;
+    // Функция отжатия мыши
+    // Если флажок движения true, то проверяю, закрыт ли был до этого попап, в зависимости от стороны я расписал немного разное взаимодейстие. Если он был закрыт, достаточно лишь нажать на попап и он раскроется. Если свайпнули больше чем на 70 пикселей, попап закрывается
+    let onInfoMouseup = function (evt) {
+        if (isMoving) {
+            info.style.transition = '0.3s ease';
+            if (!closed) {
+                if (translate > 70) {
+                    translate = translateNumber;
+                    info.style.transform = `translateY(${translate}px)`;
+                    // info.style.transform = '-100%';
+                    closed = true;
+                }
+                else{
+                    info.style.transform = 'translateY(0px)';
+                    translate = 0 ;
+                }
+            }
+            else{
+                info.style.transform = 'translateY(0px)';
+                translate = 0 ;
+                closed = false;   
+            }
+            isMoving = false;
+        }
+    };
+    // Функция движения мыши
+    // Проверяю флажок движения, если он true, то смотрю, на сколько изменилось положение мыши и меняю положение блока по этому значению
+    let onInfoMousemove = function (evt) {
+        if (isMoving) {
+            curCoord = evt.clientY || evt.touches[0].pageY;
+            translate+=(curCoord - startCoord);
+            // document.write(evt.clientY, ' --- ',startCoord);
+            if (translate > 0 && translate < translateNumber) {
+                info.style.transform = `translateY(${translate}px)`;
+            }
+            startCoord = curCoord;
+        }
+    };
+    // Функция нажатия мыши
+    // При клике отчитываю первые координаты мыши и ставлю флажок, что движение начато
+    let onInfoMousedown = function (evt) {
+        startCoord = evt.clientY || evt.touches[0].pageY;
+        info.style.transition = '0s';
+        isMoving = true;
+    };
+    info.addEventListener('mousedown', onInfoMousedown);
+    info.addEventListener('mousemove', onInfoMousemove);
+    window.addEventListener('mouseup', onInfoMouseup);
+    info.addEventListener('mouseleave',onInfoMouseup);
+
+    info.addEventListener('touchstart', onInfoMousedown);
+    info.addEventListener('touchmove', onInfoMousemove);
+    window.addEventListener('touchend', onInfoMouseup);
+
     ymaps.ready(init);
     function init(){
         let from, where, firstGeoObject, secondGeoObject;
@@ -24,6 +106,13 @@
             function(result) {
                 firstGeoObject = result.geoObjects.get(0);
                     document.querySelector('#from').classList.remove('invalid');
+                    if (secondGeoObject && firstGeoObject) {
+                        total.classList.remove('dn');
+                        translateNumber = info.getBoundingClientRect().height - 40;
+                        if (window.navigator.userAgent.indexOf('Android') > -1) {
+                            translateNumber = 320;
+                        }
+                    }
                     firstGeoObject.options.set('iconLayout', 'default#image');
                     firstGeoObject.options.set('iconImageHref', 'images/darklocation.svg');
                     firstGeoObject.options.set('iconImageSize', [38, 38]);
@@ -48,6 +137,13 @@
             }).then(function (res) {
                     firstGeoObject = res.geoObjects.get(0);
                     document.querySelector('#from').classList.remove('invalid');
+                    if (secondGeoObject && firstGeoObject) {
+                        total.classList.remove('dn');
+                        translateNumber = info.getBoundingClientRect().height - 40;
+                        if (window.navigator.userAgent.indexOf('Android') > -1) {
+                            translateNumber = 320;
+                        }
+                    }
                     firstGeoObject.options.set('iconLayout', 'default#image');
                     firstGeoObject.options.set('iconImageHref', 'images/darklocation.svg');
                     firstGeoObject.options.set('iconImageSize', [38, 38]);
@@ -67,6 +163,13 @@
             }).then(function (res) {
                     secondGeoObject = res.geoObjects.get(0);
                     document.querySelector('#where').classList.remove('invalid');
+                    if (secondGeoObject && firstGeoObject) {
+                        total.classList.remove('dn');
+                        translateNumber = info.getBoundingClientRect().height - 40;
+                        if (window.navigator.userAgent.indexOf('Android') > -1) {
+                            translateNumber = 320;
+                        }
+                    }
                     secondGeoObject.options.set('iconLayout', 'default#image');
                     secondGeoObject.options.set('iconImageHref', 'images/violetlocationmap.svg');
                     secondGeoObject.options.set('iconImageSize', [38, 38]);
